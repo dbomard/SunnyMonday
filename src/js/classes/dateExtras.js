a046d41751e59a8ef1b18c59932e4b7795d6eb76
BOMARD David <david.bomard@ville-bourges.fr>
2024-01-06 10:53:26 +0100

    Remove unnecessary function

--- src/js/classes/dateExtras.js
+++ src/js/classes/dateExtras.js
@@ -14,10 +14,3 @@ Date.prototype.getWeek = function() {
     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
                           - 3 + (week1.getDay() + 6) % 7) / 7);
   }
-  
-  // Returns the four-digit year corresponding to the ISO week of the date.
-  Date.prototype.getWeekYear = function() {
-    var date = new Date(this.getTime());
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    return date.getFullYear();
-  }
