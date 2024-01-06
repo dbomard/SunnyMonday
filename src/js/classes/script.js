708db4df104ad016eeed7f7d2f852ad08328e8ff
BOMARD David <david.bomard@ville-bourges.fr>
2024-01-06 12:17:01 +0100

    Add creation of 4 teams

--- src/js/classes/script.js
+++ src/js/classes/script.js
@@ -1,6 +1,11 @@
 import { Year } from "./year.js";
+import { Team } from "./team.js";
 
 var year = new Year();
+var teamA = new Team("Equipe Verte", "#53cc08");
+var teamB = new Team("Equipe Orange", "#f59d05");
+var teamC = new Team("Equipe Jaune", "#eaf20a");
+var teamD = new Team("Equipe Bleue", "#69befa");
 
 function changeYear(event) {
   let newYear = event.target.value;
