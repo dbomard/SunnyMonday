cc95e1892dd72b4f3fcd7b10978bb1316918774d
dbomard <david.bomard@laposte.net>
2024-01-07 23:05:21 +0100

    add fetching les jours fériés

--- src/js/classes/year.js
+++ src/js/classes/year.js
@@ -1,19 +1,47 @@
 import "./dateExtras.js";
+import { weekTypes } from "./week.js";
+
+async function downloadOffDays(year) {
+  const response = await fetch(
+    `https://calendrier.api.gouv.fr/jours-feries/metropole/${year}.json`);
+  return response.text();
+}
 
 export class Year {
   #year;
-  #weeks;
+  #weeksNumber;
+  #workingWeeks;
+  #offDays;
 
   constructor() {
     let date = new Date();
     this.#year = date.getFullYear();
+    this.#updateOffDays();
     this.#updateWeekNumbers();
+    this.#updateWorkingWeeks();
+  }
+
+  #updateWorkingWeeks() {
+    //TODO : compléter méthode updateWorkingWeeks
+    this.#workingWeeks = new Array(this.weeksNumber);
+
+  }
+
+  #updateOffDays() {
+    //TODO : compléter Méthode getOffDays
+    downloadOffDays(this.year)
+      .then((response) => this.#offDays = JSON.parse(response))
+      .then(() => console.log(`Jours fériés mis à jour:\n${JSON.stringify(this.#offDays)}`));
   }
 
   #updateWeekNumbers() {
     let date = new Date(this.#year, 11, 28);
-    this.#weeks = date.getWeek();
-    console.log(`${this.#weeks} semaines dans l'année ${this.#year}`);
+    this.#weeksNumber = date.getWeek();
+    console.log(`${this.#weeksNumber} semaines dans l'année ${this.#year}`);
+  }
+
+  get offDays() {
+    return this.#offDays;
   }
 
   get year() {
@@ -24,9 +52,10 @@ export class Year {
     console.log(`Année changée\n`);
     this.#year = newYear;
     this.#updateWeekNumbers();
+    this.#updateOffDays();
   }
 
-  get weeks() {
-    return $this.this.#weeks;
+  get weeksNumber() {
+    return $this.this.#weeksNumber;
   }
 }
