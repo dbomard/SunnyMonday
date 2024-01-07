3304703ff536b13e7947f24966f3fa8a000dc1ba
dbomard <david.bomard@laposte.net>
2024-01-07 23:07:07 +0100

    add typical weeks

--- src/js/classes/week.js
+++ src/js/classes/week.js
@@ -1,3 +1,56 @@
-class Week{
-    
+export const weekTypes = {
+    "open": {
+        "monday": true,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": true,
+        "sunday": false
+    },
+    "holidays": {
+        "monday": false,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": true,
+        "sunday": false
+    },
+    "typeA": {
+        "monday": false,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": true,
+        "sunday": false
+    },
+    "typeB": {
+        "monday": false,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": true,
+        "sunday": false
+    },
+    "typeC": {
+        "monday": true,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": false,
+        "sunday": false
+    },
+    "typeD": {
+        "monday": true,
+        "tuesday": true,
+        "wednesday": true,
+        "thursday": true,
+        "friday": true,
+        "saturday": false,
+        "sunday": false
+    },
 }
