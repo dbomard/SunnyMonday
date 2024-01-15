d77763e04ed3514cf90d8ae16f9f16536dbb9327
dbomard <david.bomard@gmail.com>
2024-01-15 23:16:01 +0100

    Correction du passage de paramètres

--- src/js/sockets.js
+++ src/js/sockets.js
@@ -1,16 +1,17 @@
 export { getHolidays, getPublicHolidays }
 
 async function getHolidays(year = 2024) {
+    let previousYear = parseInt(year) - 1;
+    let nextYear = parseInt(year) + 1;
     const baseUrl = new URL("https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-calendrier-scolaire/records");
-    const params = new URLSearchParams({
-        limit: '20',
-        refine: 'population: "-"',
-        refine: 'population: "Élèves"',
-        refine: 'location: "Orléans-Tours"',
-        refine: `annee_scolaire: "${year}-${year + 1}"`,
-        refine: `annee_scolaire: "${year - 1}-${year}"`
-    })
-    const result = await fetch(`${baseUrl}?${params}`);
+    const params = new URLSearchParams();
+    params.append("limit", '20');
+    params.append("refine", 'population:"-"');
+    params.append("refine", 'population:"Élèves"');
+    params.append("refine", 'location:"Orléans-Tours"');
+    params.append("refine", `annee_scolaire:"${year}-${nextYear}"`);
+    params.append("refine", `annee_scolaire:"${previousYear}-${year}"`);
+    const result = await fetch(`${baseUrl}?${params.toString()}`);
     const holidays = await result.json();
     return holidays.results;
 }
