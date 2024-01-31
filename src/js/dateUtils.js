7c16ce69c4977fe7f493f669eeb851a9ba1e684c
BOMARD David <david.bomard@ville-bourges.fr>
2024-01-31 18:24:47 +0100

    Ajout des attribut "holiday", "weekend" et "publicDay" à Date

--- src/js/dateUtils.js
+++ src/js/dateUtils.js
@@ -4,22 +4,93 @@
 
 // Returns the ISO week of the date.
 Date.prototype.getWeek = function () {
-    var date = new Date(this.getTime());
-    date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
-    var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-        - 3 + (week1.getDay() + 6) % 7) / 7);
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
 };
 
-// Retourne une nouvelle Date daysCount jours plus tard
+/**
+ *
+ * @param {integer} daysCount - Nombre de jours à ajouter
+ * @returns {Date} Nouvel objet Date
+ */
 Date.prototype.addDays = function (daysCount) {
-    const newDate = new Date(
-        this.getFullYear(),
-        this.getMonth(),
-        this.getDate() + daysCount);
-    return newDate;
-}
+  const newDate = new Date(
+    this.getFullYear(),
+    this.getMonth(),
+    this.getDate() + daysCount
+  );
+  return newDate;
+};
+
+/**
+ *
+ * @returns {Date} A new Date objet that is a copy of current date
+ */
+Date.prototype.getCopy = function () {
+  return new Date(this.getTime);
+};
+
+/**
+ * @param {boolean} holiday - true = Set that day as holiday
+ */
+Date.prototype.setHoliday = function (holiday = true) {
+  this.holiday = holiday;
+};
+
+/**
+ * @returns {boolean} true if a holiday day
+ */
+Date.prototype.getHoliday = function () {
+  if (this.holiday === undefined || this.holiday === false) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * @param {boolean} weekend - true = Set that day as part of weekend
+ */
+Date.prototype.setWeekend = function (weekend = true) {
+  this.weekend = weekend;
+};
+
+/**
+ * @returns {boolean} true if a weekend day
+ */
+Date.prototype.getWeekend = function () {
+  if (this.weekend === undefined || this.weekend === false) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * @param {boolean} publicDay - true = Set that day as public day
+ */
+Date.prototype.setPublicDay = function (publicDay = true) {
+  this.publicDay = publicDay;
+};
+
+/**
+ * @returns {boolean} true if a public day
+ */
+Date.prototype.getPublicDay = function () {
+  if (this.publicDay === undefined || this.publicDay === false) {
+    return false;
+  }
+  return true;
+};
