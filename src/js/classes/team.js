44d469ea5c6db1834b128216c4c844d56cbd89c7
david bomard <david.bomard@laposte.net>
2024-02-02 14:10:27 +0100

    Update class Team

--- src/js/classes/team.js
+++ src/js/classes/team.js
@@ -4,17 +4,33 @@ export class Team {
   #name;
   #color;
   #pattern;
+  #days
 
   /**
    *
    * @param {string} name
    * @param {Array} weeksPattern
    * @param {string} color 
+   * @param {Array.<date>} days 
    */
   constructor(name, weeksPattern, color = "#cfcfcf") {
     this.#name = name;
     this.#pattern = weeksPattern;
     this.#color = color;
+    this.#days = new Array();
+  }
+
+  resetDays() {
+    this.#days = new Array();
+  }
+
+  addDay(date, patternIndex) {
+    let newDay = date.getCopy();
+    this.#days.push(newDay);
+  }
+
+  get days() {
+    return this.#days;
   }
 
   get name() {
