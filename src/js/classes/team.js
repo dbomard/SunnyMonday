be56f612a2991fd1025f9c9754eb7f1882e86a77
dbomard <david.bomard@gmail.com>
2024-02-02 23:07:02 +0100

    complété fonction addDay

--- src/js/classes/team.js
+++ src/js/classes/team.js
@@ -24,13 +24,54 @@ export class Team {
     this.#days = new Array();
   }
 
+  /**
+   * 
+   * @param {Date} date 
+   * @param {integer} patternIndex 
+   */
   addDay(date, patternIndex) {
+    let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    /**@type {Date} newDay */
     let newDay = date.getCopy();
     if (!newDay.holiday) {
-      let index = patternIndex % this.#pattern.lenght;
+      let index = patternIndex % this.#pattern.length;
       let week = this.#pattern[index];
+      let dayName = days[newDay.getDay()];
+      let working = week[dayName];
+      newDay.setWeekend(!working);
+      console.log(`${newDay.toLocaleDateString()} : ${newDay.getWeekend()}`);
+    } else {
+      newDay.setWeekend(newDay.getDay() === 0 || newDay.getDay() === 1);
     }
+
     this.#days.push(newDay);
+  }
+
+  workingDaysCount() {
+    let count = 0;
+    for (let day of this.#days) {
+      if (!day.getWeekend()) {
+        count++;
+      }
+    }
+    count -= 25;
+    return count;
+  }
+
+  oneDayWeekends() {
+    return 1;
+  }
+
+  saturdaySundayWeekends() {
+    return 2;
+  }
+
+  sundayMondayWeekends() {
+    return 2;
+  }
+
+  threeDaysWeekends() {
+    return 3;
   }
 
   get days() {
